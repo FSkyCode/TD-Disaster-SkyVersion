804cac2981047a2dbee98b3a54e37291a0866b51
FSkyCode <fskycode@gmail.com>
2025-12-26 05:40:14 -0500

    Actualizar basic.js

--- js/basic.js
+++ js/basic.js
@@ -1,6 +1,6 @@
-const document.getElementById('A');
+const Presionar = document.getElementById('A');
 
-A.addEventListener("click", redirigirHtml("html/personajes/Tails.html));
+Presionar.addEventListener("click", redirigirHtml("html/personajes/Tails.html));
 
 function xD(){
 
