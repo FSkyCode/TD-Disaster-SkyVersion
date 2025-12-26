d5e284683a1b823afdfb319972efa297c89b1848
FSkyCode <fskycode@gmail.com>
2025-12-26 03:59:04 -0500

    Actualizar basic.js

--- js/basic.js
+++ js/basic.js
@@ -3,5 +3,9 @@ funcion redirigirHtml(ruta) {
  windows.location.href = ruta;
 }
 
+funcion abrirNuevaPag(ruta) {
+ window.open(ruta);
+}
+
 document.getElementById("btnTails").addEventListener("click", function () { redirigirHtml("html/personajes/Tails.html); }); 
 </script>
