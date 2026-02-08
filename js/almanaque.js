ca6982b893d497c2116b74c261d46ead98d8d9e7
FSkyCode <fskycode@gmail.com>
2026-02-08 00:39:45 -0500

    Actualizar almanaque.js

--- js/almanaque.js
+++ js/almanaque.js
@@ -1,1 +1,27 @@
-x
+// Variables
+
+const Tails = document.getElementById("btnTails");
+const Nya = document.getElementById("btnNya");
+const UmbrellaFight = document.getElementById("btnUmbrellaFight");
+
+// Botones
+
+Tails.addEventListener("click", function () { redirigirHtml("html/personajes/Tails.html");
+});
+
+Nya.addEventListener("click", function () { redirigirHtml("html/personajes/Nya.html");
+});
+
+UmbrellaFight.addEventListener("click", function () { redirigirHtml("html/personajes/UmbrellaFight.html");
+});
+
+
+// Funciones
+
+function redirigirHtml(ruta) {
+    window.location.href = ruta;
+}
+
+function abrirNuevaPag(ruta) {
+    window.open(ruta);
+}
